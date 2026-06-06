92198327cb64e3851e844eb154347a124c494103
Mohd Shakib <shakilahmad5613@gmail.com>
2026-06-06 23:27:58 +0530

    Updated backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "hhttps://social-app-z0bd.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
